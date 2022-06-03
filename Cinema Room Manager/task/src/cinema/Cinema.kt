8d2b3c94b80b819856f13f5513b092e51be3e9e6
ahmedhosnypro <ahmedhosnypro@gmail.com>
2022-06-03 08:49:37 +0200

    Sold

--- Cinema Room Manager/task/src/cinema/Cinema.kt
+++ Cinema Room Manager/task/src/cinema/Cinema.kt
@@ -1,9 +1,12 @@
 package cinema
 
+import cinema.Profit.calculateProfit
+
 object Cinema {
     @JvmStatic
     fun main(args: Array<String>) {
         val cinemaRoom = CinemaRoom()
         println(cinemaRoom)
+        calculateProfit()
     }
 }
